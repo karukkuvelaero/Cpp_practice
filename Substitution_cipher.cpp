99f56fb73b78a4c8d38376e7e9786a58cede8259
Karukkuvelaero <d.karukkuvelraj@gmail.com>
2024-11-09 06:40:35 +0530

    Cipher_started

---
+++ Substitution_cipher.cpp
@@ -0,0 +1,26 @@
+/*
+author=@karukkuvelaero
+file_name = Substitution_Cipher
+function = to encrypt a message using given key
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::string key = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsprqonmlkjihgfedcba";
+
+    std::string message;
+    std::cout<<"Enter the message"<<std::endl;
+    getline(std::cin,message);
+    std::string encrypted;
+
+    std::cout<<message;
+    
+    for (int i=0; i<<message.size; i++)
+    {
+        char temp = 
+    }
+}
