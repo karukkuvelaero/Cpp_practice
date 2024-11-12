c128bd5172541bcc3d554fd5c8115cadc7093f9d
Karukkuvelaero <d.karukkuvelraj@gmail.com>
2024-11-12 22:52:48 +0530

    Cipher_ended

--- Substitution_cipher.cpp
+++ Substitution_cipher.cpp
@@ -18,9 +18,43 @@ int main()
     std::string encrypted;
 
     std::cout<<message;
+
     
-    for (int i=0; i<<message.size; i++)
+    for (int i=0; i<message.size(); i++)
     {
-        char temp = 
+        char temp = message[i];
+        int key_pos = alphabets.find(temp);
+        
+        if (key_pos != std::string::npos)
+        {
+            encrypted.push_back(key[key_pos]);
+        }
+        else
+        {
+            encrypted.push_back(temp);
+        }
+        
     }
+
+    std::cout<<"Encrypted message is "<<encrypted<<std::endl;
+
+std::string decrypted;
+
+for (int i=0; i<encrypted.size(); i++)
+    {
+        char temp = encrypted[i];
+        int key_pos = key.find(temp);
+        if (key_pos != std::string::npos)
+        {
+            decrypted.push_back(alphabets[key_pos]);
+        }
+        else
+        {
+            decrypted.push_back(temp);
+        }
+        
+    
+    }
+    std::cout<<"Decrypted message is "<<decrypted<<std::endl;
+    return 0;
 }
